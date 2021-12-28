4758fef4012d1b9844a64a83c4f0bff23b9b4d7d
mostafa017 <96733876+mostafa017@users.noreply.github.com>
2021-12-28 19:33:18 +0200

    before mostafa

--- Dying To Live/Assets/Scripts/PlayerController.cs
+++ Dying To Live/Assets/Scripts/PlayerController.cs
@@ -2,66 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-	public float moveSpeed;
-	public float jumpHeight;
-	public bool isFacingRight;
-	public KeyCode Spacebar;
-	public KeyCode L;
-	public KeyCode R;
-	public Transform groundCheck;
-	public float groundCheckRadius;
-	public LayerMask whatIsGround;
-	private bool grounded;
-
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(Spacebar) )
-		{
-			Jump();
-
-		}
-		
-		if (Input.GetKey(L))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
-			if (isFacingRight)
-			{
-				flip();
-			}
-			isFacingRight = false;
-
-		}
-		
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    public bool isFacingRight;
+    public KeyCode Spacebar;
+    public KeyCode L;
+    public KeyCode R;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    private Animator anim;
 
 
-		if (Input.GetKey(R))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			if (!isFacingRight)
-			{
-				flip();
-				isFacingRight = true;
 
-			}
-		}
+    void flip()
+    {
+        transform.localScale = new Vector3(-(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+    }
+    void Start()
+    {
+        isFacingRight = true;
+        anim = GetComponent<Animator>();
+    }
 
 
-	}
-	void flip()
-	{
-		transform.localScale = new Vector3(-(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-	}
-	void Jump()
-	{
-		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
-	}
-	void FixedUpdate()
-	{
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-	}
+    void FixedUpdate()
+    {
 
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(Spacebar) && grounded)
+        {
+
+            Jump();
+
+        }
+        if (Input.GetKey(L))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (isFacingRight)
+            {
+                flip();
+                isFacingRight = false;
+
+            }
+
+
+
+        }
+        if (Input.GetKey(R))
+        {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (!isFacingRight)
+            {
+                flip();
+                isFacingRight = true;
+
+
+            }
+
+        }
+        anim.SetBool("ground", grounded);
+        anim.SetFloat("speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+    }
 }
+
