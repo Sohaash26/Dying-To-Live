cdb076e19d823db656f0ad87d6e83a4a8b78f34f
Sohaash26 <96625328+Sohaash26@users.noreply.github.com>
2021-12-29 17:02:52 +0200

    Adding UI Menus
    Still need to fix.
    the main problem is the Camera.

---
+++ Dying To Live/Assets/Scripts/UI Scripts/OptionUI.cs
@@ -0,0 +1,21 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public void ExitPause()
+    {
+        PlayerCharacter.PlayerInstance.Unpause();
+    }
+
+    public void RestartLevel()
+    {
+        ExitPause();
+        SceneController.RestartZone();
+    }
+}
+*/
---
+++ Dying To Live/Assets/Scripts/UI Scripts/StartUI.cs
@@ -0,0 +1,23 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace DyingToLive
+{
+    public class StartUI : MonoBehaviour
+    {
+
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
+*/
